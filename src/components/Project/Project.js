6290db64c0652b22557eb0f630e317bbdbf522aa
EvPng <psy03che@live.com>
2021-01-21 14:38:02 -0800

    fix broken links

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -9,7 +9,7 @@ class Project extends React.Component {
             <div className="Project">
                 <h2>{this.props.framework}</h2>
                 <a className='image-container' href={this.props.url} target="_blank" rel="noreferrer noopener">
-                    <img src={this.props.imageSrc} alt={this.props.title} />
+                    <img src={`./${this.props.imageSrc}`} alt={this.props.title} />
                     <div className='overlay'>
                         <div className='text-on-image'>{this.props.title}</div>
                         <br />
--- src/components/data.js
+++ src/components/data.js
@@ -47,7 +47,7 @@ const data = [
     {
         id: 0,
         title: 'CAMLab Tea House',
-        imageSrc: '/img/tea.png',
+        imageSrc: 'img/tea.png',
         intro: 'Interactive Documentary with WebGL',
         framework: 'React, Three.js, Cannon.js, Firebase',
         url: 'https://camlab-tea-house.web.app/'
@@ -55,7 +55,7 @@ const data = [
     {
         id: 1,
         title: 'Wikipedia Dramatizer',
-        imageSrc: '/img/wiki.png',
+        imageSrc: 'img/wiki.png',
         intro: 'Web tool to examine the edit history of Wikipedia and expose contested areas of knowledge',
         framework: 'D3.js, p5.js, Wikipedia API',
         url: 'https://evpng.github.io/WikipediaDramatizer/',
@@ -64,7 +64,7 @@ const data = [
     {
         id: 2,
         title: 'Western Chamber Vase',
-        imageSrc: '/img/vase.png',
+        imageSrc: 'img/vase.png',
         intro: 'Interactive Digitally Restored Antique Vase',
         framework: 'Three.js, Scanned point cloud with texture',
         url: 'https://evpng.github.io/CAMLab-western-chamber/'
@@ -72,7 +72,7 @@ const data = [
     {
         id: 3,
         title: 'CAMLab Publication Space',
-        imageSrc: '/img/pub.png',
+        imageSrc: 'img/pub.png',
         intro: 'Database of CAMLab Publications',
         framework: <a href="https://www.figma.com/proto/PQpEVaYeidEa0LeET58rNm/CAMLab-Publication?node-id=1%3A116&viewport=38%2C-2670%2C0.40708568692207336&scaling=scale-down" target="_blank" rel="noreferrer">React, CMS - Work in progress, see prototype here</a>,
         url: 'https://evpng.github.io/camlab-publication/',
@@ -81,7 +81,7 @@ const data = [
     {
         id: 4,
         title: 'Word Smith',
-        imageSrc: '/img/wordsmith.png',
+        imageSrc: 'img/wordsmith.png',
         intro: 'Find words of rhymes and words of topics',
         framework: 'AJAX, Datamuse API',
         url: 'https://evpng.github.io/word-smith/'
@@ -89,7 +89,7 @@ const data = [
     {
         id: 5,
         title: 'Chore Bot!',
-        imageSrc: '/img/chorebot.png',
+        imageSrc: 'img/chorebot.png',
         intro: 'Game',
         framework: 'Simple web game excercise',
         url: 'https://evpng.github.io/chore-bot/'
